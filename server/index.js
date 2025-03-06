bd7ee863c64fcb55b653e17bb0e6c81d6f72fd00
skyehakomaa <skyehakomaa@gmail.com>
2025-03-06 11:47:43 +0200

    add socket.io and serve dist

--- server/index.js
+++ server/index.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 const PORT = process.env.PORT || 5000;
 
-const server = app.listen(PORT, () => {
-  console.log("server is running on port", server.address().port);
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
 });
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`listening on *:${PORT}`);
+});
+
+
