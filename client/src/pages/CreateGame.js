7b0402488d3e9e531ac1bfaaecd83e1fc3cd6ded
skyehakomaa <skyehakomaa@gmail.com>
2025-03-06 16:11:21 +0200

    create join info

--- client/src/pages/CreateGame.js
+++ client/src/pages/CreateGame.js
@@ -1,16 +1,21 @@
 export default function CreateGame() {
     const self = this
 
-    self.info = () => {
-        alert("Info!")
+    self.displayInfo = () => {
+        self.buttons.classList.toggle("hidden")
+        self.info.classList.toggle("hidden")
     }
 
     return `    <div class="create-game page-wrap">
       <header class="center">Tetris Game</header>
       <main class="content-wrap, center">
-        <div class="button-container">
-          <button id="join" onclick="self.info">Join Game</button>
+        <div class="button-container" :ref="self.buttons">
+          <button id="displayInfo" onclick="self.displayInfo">Join Game</button>
           <button id="create">Create Game</button>
+        </div>
+        <div class="button-container hidden" :ref="self.info">
+            <p>Info</p>
+            <button id="back" onclick="self.displayInfo">Go back</button>
         </div>
       </main>
       <footer class="center">Wee-woo!</footer>
