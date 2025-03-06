81bcc5513f1eb18292095b2583f29ae6f573ba17
skyehakomaa <skyehakomaa@gmail.com>
2025-03-06 18:46:54 +0200

    create redirect to lobby

--- client/src/pages/CreateGame.js
+++ client/src/pages/CreateGame.js
@@ -6,16 +6,20 @@ export default function CreateGame() {
         self.info.classList.toggle("hidden")
     }
 
+    self.toLobby = () => {
+      window.location = "./chat"
+    }
+
     return `    <div class="create-game page-wrap">
       <header class="center">Tetris Game</header>
       <main class="content-wrap, center">
         <div class="button-container" :ref="self.buttons">
-          <button id="displayInfo" onclick="self.displayInfo">Join Game</button>
-          <button id="create">Create Game</button>
+          <button id="display" onclick="self.displayInfo">How to join a game?</button>
+          <button id="create" onclick="self.toLobby">Create Game</button>
         </div>
         <div class="button-container hidden" :ref="self.info">
             <p>Info</p>
-            <button id="back" onclick="self.displayInfo">Go back</button>
+            <button id="display" onclick="self.displayInfo">Go back</button>
         </div>
       </main>
       <footer class="center">Wee-woo!</footer>
