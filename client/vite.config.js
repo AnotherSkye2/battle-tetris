67833854a322c13280c7c439bb911ea40ebf3a5a
skyehakomaa <skyehakomaa@gmail.com>
2025-03-07 20:56:36 +0200

    create react init

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,7 @@
-import vitePluginSocketIO from 'vite-plugin-socket-io'
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-const socketEvents = (io, socket) => {
-  console.log('socket.io - connection');
-  socket.on('disconnect', () => {
-    console.log(`socket.io - socket.id \`${socket.id}\` disconnected`)
-  })
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
-  });
-}
-
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vitePluginSocketIO({socketEvents})]
+  plugins: [react()],
 })
