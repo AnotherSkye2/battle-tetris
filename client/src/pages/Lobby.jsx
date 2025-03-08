1e855afcb8213f216a1d0fd7f46ed7269169d05b
skyehakomaa <skyehakomaa@gmail.com>
2025-03-08 21:09:19 +0200

    create users and username input

--- client/src/pages/Lobby.jsx
+++ client/src/pages/Lobby.jsx
@@ -1,25 +1,42 @@
 import {useState, useEffect} from 'react'
 import Chat from "../components/Chat"
+import NamePopup from "../components/NamePopup"
 import { socket } from '../socket';
 import { useParams } from "react-router";
 
 export default function Lobby() {
+  const [link, setLink] = useState("")
   const [inputValue, setInputValue] = useState("")
   const [chatMessages, setChatMessages] = useState([])
-  const [socketIds, setSocketIds] = useState([])
-  // const [users, setUsers] = useState([])
+  const [users, setUsers] = useState([])
+  const [userHasName, setUserHasName] = useState(false)
   const {roomId} = useParams();
+
+
 
   const copyToClipboard = (e) => {
     async function copyPageUrl() {
       try {
-        await navigator.clipboard.writeText(inputValue);
+        await navigator.clipboard.writeText(link);
         console.log('Page URL copied to clipboard');
       } catch (err) {
         console.error('Failed to copy: ', err);
       }
     }
     copyPageUrl().then(() => {e.target.innerHTML = "Copied!"})
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(inputValue)
+    if (!inputValue) {
+      setInputValue(`Guest ${new Date().getMilliseconds()}`)
+    }
+    setUserHasName(true)
+  }
+
+  const handleChange = (e) => {
+      setInputValue(e.target.value)
   }
 
   useEffect(() => {
@@ -34,22 +51,28 @@ export default function Lobby() {
   }, [chatMessages])
   
   useEffect(() => {
-    console.log("socketIds: ", socketIds)
-    socket.on("join", (newSocketId) => {setSocketIds([...socketIds, newSocketId])})
-  },[socketIds])
+    socket.on("join", (user) => {setUsers([...users, user])})
+    return () => {
+      socket.off("join")
+    }
+  },[users])
 
   useEffect(() => {
-    setInputValue(`http://localhost:5173/lobby/${roomId}`)
-    socket.connect();
-    socket.emit('join', roomId, (roomSocketIds) => {
-      console.log("roomSocketIds, socket.id:", roomSocketIds, socket.id)
-      setSocketIds([...socketIds, ...roomSocketIds])
-    });
-    return () => {
-      socket.emit('leave', roomId)
-      socket.disconnect();
+    if (userHasName) {
+      setLink(`http://localhost:5173/lobby/${roomId}`)
+      socket.connect();
+      socket.username = inputValue;
+      console.log("socket.username:", socket.username)
+      socket.emit('join', roomId, inputValue, (roomUsers) => {
+        console.log("roomSocketIds, socket.id:", roomUsers, socket.id)
+        setUsers(u => [...u, ...roomUsers])
+      });
+      return () => {
+        socket.emit('leave', roomId)
+        socket.disconnect();
+      }
     }
-  }, [])
+  }, [userHasName])
 
   return(
     <div className="create-game page-wrap">
@@ -60,12 +83,17 @@ export default function Lobby() {
         </div>
         <main className="center" >
           <div className="lobby-container" >
-            <input type="text" id="display" value={inputValue} readOnly></input>
+            <input type="text" id="display" value={link} readOnly></input>
             <button id="create" onClick={copyToClipboard}>Copy!</button>
           </div>
         </main>
+        <div>
+          <h4>Users:</h4>
+          <ul id='users'>{users.map((message, i) => <li key={i}>{Object.keys(message)[0]}</li>)}</ul>
+        </div>
       </div>
       <footer className="center">Wee-woo!</footer>
+      <NamePopup handleSubmit={handleSubmit} handleChange={handleChange}/>
     </div>
   )
 }
--- server/index.js
+++ server/index.js
@@ -27,23 +27,29 @@ io.on('connection', (socket) => {
     console.log('user disconnected');
   });
 
-  socket.on('join', (roomId, callback) => {
+  socket.on('join', (roomId, username, callback) => {
     console.log("roomId:", roomId)
     if(!socket.rooms.has(roomId)){
+      socket.username = username
       socket.join(roomId)
-      socket.to(roomId).emit('chat message', `A player (${socket.id}) has joined!`);
+      socket.to(roomId).emit('chat message', `${socket.username} has joined!`);
       socket.on('chat message', (msg) => {
         io.to(roomId).emit('chat message', msg);
       });
+      socket.to(roomId).emit('join', {[socket.username]: socket.id});
+      
       const clients = io.sockets.adapter.rooms.get(roomId)
       console.log("clients: ", clients, socket.id)
-      socket.to(roomId).emit('join', socket.id);
-      callback([...clients])
+      const users = [...clients].map((clientId) => {
+        const socket = io.sockets.sockets.get(clientId);
+        return {[socket.username]: socket.id}
+      })
+      callback(users)
     }
   })
 
   socket.on('leave', (roomId) => {
-    socket.to(roomId).emit('chat message', `A player (${socket.id}) has left!`);
+    socket.to(roomId).emit('chat message', `${socket.username} has left!`);
     socket.removeAllListeners('chat message')
     socket.leave(roomId)
   })
