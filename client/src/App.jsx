e299114b616f8577c83d24a66d25c8e87d09fa47
skyehakomaa <skyehakomaa@gmail.com>
2025-03-09 12:46:54 +0200

    move user to game screen on pressing start

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import './App.css'
 import Home from './pages/Home'
 import Lobby from './pages/Lobby'
+import Game from './pages/Game'
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/lobby/:roomId" element={<Lobby />} />
+      <Route path="/game/:roomId" element={<Game />} />
     </Routes>
   </BrowserRouter>
   )
--- client/src/pages/Lobby.jsx
+++ client/src/pages/Lobby.jsx
@@ -43,6 +43,7 @@ export default function Lobby() {
   const handleStart = () => {
     console.log("Start!")
     sessionStorage.setItem('users', JSON.stringify(users))
+    window.location = `../game/${roomId}`
   }
 
   useEffect(() => {
