2d166728dca942a08143f333b068ac0acc501ac9
skyehakomaa <skyehakomaa@gmail.com>
2025-03-11 01:11:28 +0200

    add leaderboard sorting

--- client/src/methods/gameScore.js
+++ client/src/methods/gameScore.js
@@ -28,13 +28,30 @@ export function addScore(clearedLines,gameState){
 }
 
 export function updateScore(gameloopObject) {
+    const test = gameloopObject.userScoreElementArray
+    console.log(test)
     if (gameloopObject.gameState.gameScore > 0) {
         console.log("gameloopObject.gameState.gameScore: ", gameloopObject.gameState.gameScore, gameloopObject.gameState.gameScore)
         const scoreElementArray = gameloopObject.userScoreElementArray
+        const leaderboard = scoreElementArray[0].userScoreElement.parentNode.parentNode.parentNode
+        console.log(leaderboard)
         for (let i = 0; i < scoreElementArray.length; i++) {
             if (scoreElementArray[i].name == userName) {
                 scoreElementArray[i].userScoreElement.innerHTML = gameloopObject.gameState.gameScore + Number(scoreElementArray[i].userScoreElement.innerHTML)
             }
+        }
+        console.log("before", scoreElementArray)
+        scoreElementArray.sort((a, b) => {
+            return a.userScoreElement.innerHTML == b.userScoreElement.innerHTML
+            ? 0
+            : (Number(a.userScoreElement.innerHTML) > Number(b.userScoreElement.innerHTML) ? -1 : 1);
+        })
+        console.log("after", scoreElementArray)
+        let scoreContainerArray = scoreElementArray.map((scoreElement) => scoreElement.userScoreElement.parentNode.parentNode)
+    
+        leaderboard.innerHTML = ""
+        for (let i = 0; i < scoreContainerArray.length; i++) {
+            leaderboard.appendChild(scoreContainerArray[i])
         }
         // gameloopObject.socket.emit("score", roomId, score)
     } else {
